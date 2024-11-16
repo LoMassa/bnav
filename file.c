eeb51cc58910fc9e9206e8711f9dc1a04a2c07c7
LoMassa <165552756+LoMassa@users.noreply.github.com>
2024-11-16 12:46:57 +0100

    Update file.c

--- file.c
+++ file.c
@@ -7,19 +7,19 @@ void crea_mat(int v[100]){      //inserimento primo utente e creazione della gri
   for(int i = 0; i<6; i++){
     do{
         printf("Inserimento: ");
-        scanf("%s", ins);
-    while( !( 'A' <= ins[0] <= 'J') || !(ins[1] == '1' && !(ins[2] == '0' || ins[2] == '\0' )) || !('1' <= ins[1] <= '9'));
+        scanf("%s", &ins);
+    }while( !( 'A' <= ins[0] <= 'J') || !(ins[1] == '1' && !(ins[2] == '0' || ins[2] == '\0' )) || !('1' <= ins[1] <= '9'));
         do{
           printf("Inserire 1 per mettere la nave in verticale, 2 per inserirla in orizzontale");
           scanf("%d", &scelta);
-        }while(!(scelta == 1 || scelta == 2);
+        }while(!(scelta == 1 || scelta == 2));
         if(scelta = 1);
         
           
         
     }
 }
-}
+
 void main(){
 
 }
