880ca6a819a382c3b1e0bb8f5deb3f9c021a5834
LoMassa <165552756+LoMassa@users.noreply.github.com>
2024-11-16 12:45:45 +0100

    corretto
    cambia errori (stray)

--- file.c
+++ file.c
@@ -8,7 +8,7 @@ void crea_mat(int v[100]){      //inserimento primo utente e creazione della gri
     do{
         printf("Inserimento: ");
         scanf("%s", ins);
-    }while( !( ‘A’ <=ins[0] <= ‘J’) || !(ins[1] == ‘1’ && !(ins[2] == ‘0’ || ins[2] == ‘\0’ )) || !('1' <= ins[1] <= '9'));
+    while( !( 'A' <= ins[0] <= 'J') || !(ins[1] == '1' && !(ins[2] == '0' || ins[2] == '\0' )) || !('1' <= ins[1] <= '9'));
         do{
           printf("Inserire 1 per mettere la nave in verticale, 2 per inserirla in orizzontale");
           scanf("%d", &scelta);
