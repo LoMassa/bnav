3d91a529fd05f619169e0a8bd7c8bc54c5b00b3e
LoMassa <165552756+LoMassa@users.noreply.github.com>
2024-11-16 12:42:04 +0100

    mid lezione 1

--- file.c
+++ file.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
-void inserimento(int ){
-
+void crea_mat(int v[100]){      //inserimento primo utente e creazione della griglia (matrice)
+  int colonna;
+  int riga;
+  char ins[3];
+  int scelta;
+  for(int i = 0; i<6; i++){
+    do{
+        printf("Inserimento: ");
+        scanf("%s", ins);
+    }while( !( ‘A’ <=ins[0] <= ‘J’) || !(ins[1] == ‘1’ && !(ins[2] == ‘0’ || ins[2] == ‘\0’ )) || !('1' <= ins[1] <= '9'));
+        do{
+          printf("Inserire 1 per mettere la nave in verticale, 2 per inserirla in orizzontale");
+          scanf("%d", &scelta);
+        }while(!(scelta == 1 || scelta == 2);
+        if(scelta = 1);
+        
+          
+        
+    }
+}
 }
 void main(){
 
