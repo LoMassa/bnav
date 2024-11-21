c50558518bd12e93c6195baddf51ce4c9cced630
LoMassa <165552756+LoMassa@users.noreply.github.com>
2024-11-21 22:51:35 +0100

    Update file.c
    Aggiunta della rotazione. Manca il controllo che le navi non si sovrappongano

--- file.c
+++ file.c
@@ -3,18 +3,22 @@
 void inserimento(int lunghnav, char v[][10][3]){
      //variabili
      
-     int k = 1, k1=1;          //riga, memoria di k (usata nel printf della griglia per rimuovere l'elemento alla precedente posizione)
-     int j = 1, j1=1;          //colonna, memoria di j
+     int k = 1;          //riga, memoria di k (usata nel printf della griglia per rimuovere l'elemento alla precedente posizione)
+     int j = 1;          //colonna, memoria di j
      char ins;
-     
+     int rotazione = 1;        //rotazione nave. 1 = orizzontale, 0 = verticale
+     int inizio = 1;
 
         do{
           //posizione iniziale della nave
-          if(k==1 && j==1){
-               v[0][0][0] = '[';
-               v[0][0][1] = 'x';
-               v[0][0][2] = ']';
+          if(inizio){
+               for(int o = 0; o < lunghnav; o++){
+                    v[0+o][0][0] = '[';
+                    v[0+o][0][1] = 'x';
+                    v[0+o][0][2] = ']';
           }
+          inizio = 0;
+        }
           //printf della matrice
           for(int i = 0; i<10; i++){
                for(int n = 0; n<10; n++){
@@ -29,9 +33,7 @@ void inserimento(int lunghnav, char v[][10][3]){
             scanf(" %c", &ins);
             //muovi basso
             if(ins == 's'){
-                if(k <= 9){
-                    k1 = k;
-                    j1 = j;
+                if((k + (lunghnav -1) * (1 - rotazione)) <= 9){
                     k += 1;    
                     
                 }
@@ -39,8 +41,6 @@ void inserimento(int lunghnav, char v[][10][3]){
             //muovi alto
             if(ins == 'w'){
                 if(k > 1){
-                    k1 = k;
-                    j1 = j;
                     k -= 1;
                     
                 }
@@ -48,27 +48,43 @@ void inserimento(int lunghnav, char v[][10][3]){
             //muovi sx
             if(ins == 'a'){
                 if(j > 1){
-                    j1 = j;
-                    k1 = k;
                     j-=1; 
                     
                 }
             }
             //muovi dx
             if(ins == 'd'){
-                if(j <= 9){
-                    j1 = j;
-                    k1 = k;
+                if((j + (lunghnav - 1) * rotazione) <= 9){
                     j += 1;
                     
                 }
             }
-            v[k-1][j-1][0] = '[';
-            v[k-1][j-1][1] = 'x';
-            v[k-1][j-1][2] = ']';
-            v[k1-1][j1-1][0] = '[';
-            v[k1-1][j1-1][1] = '-';
-            v[k1-1][j1-1][2] = ']';
+           //ruota la nave
+            if(ins == 'r'){
+               if (rotazione){
+                 if(k + lunghnav - 1 <= 10) 
+                    rotazione = !rotazione;  
+               }
+               else{
+                    if(j + lunghnav - 1 <= 10)
+                         rotazione = !rotazione;
+               } 
+                    
+               
+            }
+               
+          for(int i = 0; i<10; i++){
+               for(int n = 0; n<10; n++){
+                    v[i][n][0] = '[';
+                    v[i][n][1] = '-';
+                    v[i][n][2] = ']';
+               }
+     }
+            for(int n = 0; n<lunghnav; n++){
+               v[k-1 + (1 - rotazione)*n][j-1 + (rotazione)*n][0] = '[';
+               v[k-1 + (1 - rotazione)*n][j-1 + (rotazione)*n][1] = 'x';
+               v[k-1+ (1 - rotazione)*n][j-1 + (rotazione)*n][2] = ']';
+            }
             
         }while(ins != 'l');
         
@@ -89,6 +105,7 @@ void main(){
      }
      
      printf("\n");
-     inserimento(1, v);
+     inserimento(5, v);
+
      printf("Hello");
 }
